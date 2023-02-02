274fa512cd850bb201d99b64d238aa343915a859
Veli-Pekka Parkkinen <parkkinenv@gmail.com>
2023-02-02 14:41:09 +0100

    refactor wrt prep/no sub rule/submodel checks

--- ccasf.R
+++ ccasf.R
@@ -6,53 +6,144 @@ case_flipper <- function(x){
   return(out)
 }
 
-check_ccomp <- function(x,y){
-  c_asfs <- unlist(extract_asf(x))
-  if(length(c_asfs) == 1){
-    out <- check_comp_asf(y,x)
-    return(out)
-  } 
-  asfs_checked <- lapply(c_asfs, function(x) check_comp_asf(x,y))
-  out <- all(unlist(asfs_checked))
+ccheck_prep <- function(x,y){
+  tar_asfs <- unlist(extract_asf(y))
+  tar_outs <- rhs(tar_asfs)
+  tar_lhss <- lhs(tar_asfs)
+  cand_asfs <- unlist(extract_asf(x))
+  cand_outs <- rhs(cand_asfs)
+  cand_lhss <- lhs(cand_asfs)
+  names(cand_lhss) <- cand_outs
+  cand_facs <- lit_extract(cand_lhss)
+  not_subbable <- toupper(tar_outs) %in% toupper(cand_facs)
+  test_tar_lhss <- tar_lhss
+  names(test_tar_lhss) <- tar_outs
+  out <- list(target_lhss = test_tar_lhss, 
+              candidate_lhss = cand_lhss,
+              candidate_asfs = cand_asfs,
+              no_sub = not_subbable)
   return(out)
 }
 
 
-check_comp_asf <- function(x,y){
+check_ccomp <- function(x,y){
+  #cand_asfs <- unlist(extract_asf(x))
+  out <- vector("logical", 1)
+  attributes(out) <- list(why = "", 
+                          x = x, 
+                          y = y,
+                          ultimate_asfs = NULL,
+                          cand_asfs_checked = NULL)
   if(!is.inus(x, selectCases(y))){
-   return(FALSE)
-   }
-  if(length(y)==0 | is.submodel(x,y)){
-    return(TRUE)
+    out[1] <- FALSE
+    attr(out, "why") <- "x is not inus wrt selectCases(y)"
+    return(out)
   }
-  tar_asfs <- unlist(extract_asf(y))
-  tar_outs <- rhs(tar_asfs)
+  is_sm <- is.submodel(x,y)
+  c_asfcount <- unlist(strsplit(x, "<->"))
+  is_x_csf <- length(c_asfcount) > 2
+  if(!is_x_csf & is_sm){
+     out[1] <- TRUE
+     attr(out, "why") <- "x is asf and a submodel of y"
+     return(out)
+  }
+  if(is_x_csf & is_sm){
+    out[1] <- NA
+    attr(out, "why") <- "x is a csf and a submodel of y, requires further processing that I haven't coded yet"
+    return(out)
+  }
+  prepared <- ccheck_prep(x,y)
+  
+  # if(length(prepared$candidate_lhss) == 1){
+  #   out <- check_comp_asf(prepared$candidate_lhss, prepared$target_lhss)
+  #   return(out)
+  # }
+  prep_target <- prepared$target_lhss
+  asf_subms <- is.submodel(prepared$candidate_asfs, y)
+  
+  # subbed_tar_asfs <- sapply(prepared$candidate_lhss[!asf_subms], 
+  #                        function(f) check_comp_asf(f, prep_target))
+  
+  subbed_tar_asfs <- vector("character", length(prepared$candidate_lhss[!asf_subms]))
+  correct <- vector("logical", length(prepared$candidate_lhss[!asf_subms]))
+  for(i in seq_along(prepared$candidate_lhss[!asf_subms])){
+    subbed_tar_asfs[i] <- check_comp_asf(prepared$candidate_lhss[!asf_subms][i], 
+                                         prepared$target_lhss,
+                                         prepared$no_sub)
+    correct[i] <- is.submodel(prepared$candidate_asfs[i], subbed_tar_asfs[i])
+  }
+  
+  parts_correct <- c(correct, asf_subms[asf_subms])
+  names(parts_correct) <- prepared$candidate_asfs
+  out[1] <- all(parts_correct)
+  if(out[1]){
+    attr(out, "why") <- "all x asfs are submodels of expanded y asfs"
+  } else {
+    attr(out, "why") <- "some x asfs are not submodels of expanded y asfs"
+  }
+  attr(out, "ultimate_asfs") <- subbed_tar_asfs
+  attr(out, "cand_asfs_checked") <- parts_correct
+  return(out)
+}
+
+
+lit_extract <- function(lhs){
+  d <- unlist(strsplit(lhs, "\\+"))
+  out <- unlist(strsplit(d, "\\*"))
+  return(out)
+}
+
+check_comp_asf <- function(x, y, not_subbable){
+  # if(!is.inus(x, selectCases(y))){
+  #  return(FALSE)
+  #  }
+  # if(is.submodel(names(x), ogy)){
+  #   return(TRUE)
+  # }
+  # tar_asfs <- unlist(extract_asf(y))
+  # tar_outs <- rhs(tar_asfs)
+  # tar_outs_flipped <- sapply(tar_outs, case_flipper)
+  # tar_lhss <- lhs(tar_asfs)
+  # cand_lhs <- lhs(x)
+  # cand_disjs <- unlist(strsplit(cand_lhs, "\\+"))
+  # cand_conjs <- unlist(strsplit(cand_disjs, "\\*"))
+  # 
+  # cand_out <- rhs(x)
+  # outcome_match <- which(tar_outs==cand_out)
+  tar_lhss <- y
+  tar_outs <- names(y)
   tar_outs_flipped <- sapply(tar_outs, case_flipper)
-  tar_lhss <- lhs(tar_asfs)
-  cand_lhs <- lhs(x)
-  cand_disjs <- unlist(strsplit(cand_lhs, "\\+"))
-  cand_conjs <- unlist(strsplit(cand_disjs, "\\*"))
+  cand_out <- names(x)
+  outc_matches <- tar_outs %in% cand_out
+  if(!any(outc_matches)){return}
+  outcome_match <- which(tar_outs %in% cand_out)
   
-  cand_out <- rhs(x)
-  outcome_match <- which(tar_outs==cand_out)
-  
-  ultimate_lhs <- tar_lhss[outcome_match]
+  ultimate_lhs <- y[outcome_match]
   idx_sub_from <- vector("logical", length(tar_outs))
-  not_subbable <- vector("logical", length(tar_outs))
-  if(!any(toupper(tar_outs[!not_subbable]) %in% unlist(strsplit(toupper(ultimate_lhs), 
-                                                                "")))){
-    return(FALSE)
-  }
+  # if(is.null(not_subbable)){
+  #   not_subbable <- vector("logical", length(tar_outs))
+  # }
+  #    
+  # if(!any(toupper(tar_outs[!not_subbable]) %in% unlist(strsplit(toupper(ultimate_lhs), 
+  #                                                               "")))){
+  #   return(FALSE)
+  # }
+  
+  # if(!any(toupper(tar_outs[!not_subbable]) %in% unlist(strsplit(toupper(ultimate_lhs), 
+  #                                                               "")))){
+  #   return(FALSE)
+  # }
+  
   while(any(toupper(tar_outs[!not_subbable]) %in% unlist(strsplit(toupper(ultimate_lhs), 
                                                      "")))){
-    not_sub_ccon <- sapply(cand_conjs,
-                           function(y) grepl(toupper(y), 
-                                             toupper(ultimate_lhs)))
-    conj_in_ultasf <- paste0(names(which(not_sub_ccon)))
-    not_subbable <- lapply(tar_outs,
-                           function(y)
-                             grepl(toupper(y), toupper(conj_in_ultasf)))
-    not_subbable <- unlist(lapply(not_subbable, any))
+    # not_sub_ccon <- sapply(cand_conjs,
+    #                        function(y) grepl(toupper(y), 
+    #                                          toupper(ultimate_lhs)))
+    # conj_in_ultasf <- paste0(names(which(not_sub_ccon)))
+    # not_subbable <- lapply(tar_outs,
+    #                        function(y)
+    #                          grepl(toupper(y), toupper(conj_in_ultasf)))
+    # not_subbable <- unlist(lapply(not_subbable, any))
     sub_from_capmatch <- sapply(tar_outs, 
                                 function(y) 
                                   grepl(y, ultimate_lhs))
@@ -90,19 +181,22 @@ check_comp_asf <- function(x,y){
 
   
   subbed_lhs <- rreduce(getCond(selectCases(ultimate_lhs)))
-  subbed_asf <- paste0(subbed_lhs, "<->", tar_outs[outcome_match])
+  #subbed_asf <- paste0(subbed_lhs, "<->", tar_outs[outcome_match])
+  out <- paste0(subbed_lhs, "<->", tar_outs[outcome_match])
   #idx_sub_from <- unique(c(which(sub_from_capmatch), which(sub_from_capflip)))
-  new_tar_asfs <- tar_asfs
-  new_tar_asfs[outcome_match] <- subbed_asf
-  new_tar_asfs <- new_tar_asfs[!idx_sub_from]
-  if(length(new_tar_asfs) > 1){
-    new_tar_asfs <- paste0("(", new_tar_asfs, ")")
-    rec_target <- paste0(new_tar_asfs, collapse = "*")
-  } else {
-    rec_target <- new_tar_asfs
-  }
-  out <- is.submodel(x, rec_target)
-  attributes(out) <- NULL
+  
+  # new_tar_asfs <- tar_asfs
+  # new_tar_asfs[outcome_match] <- subbed_asf
+  # new_tar_asfs <- new_tar_asfs[!idx_sub_from]
+  # if(length(new_tar_asfs) > 1){
+  #   new_tar_asfs <- paste0("(", new_tar_asfs, ")")
+  #   rec_target <- paste0(new_tar_asfs, collapse = "*")
+  # } else {
+  #   rec_target <- new_tar_asfs
+  # }
+  # out <- rec_target
+  # out <- is.submodel(x, rec_target)
+  # attributes(out) <- NULL
   return(out)
 }
 
--- examples.R
+++ examples.R
@@ -3,39 +3,39 @@ y <-  "(T+R<->Y)*(A+Y*K<->C)*(C*H+C*I<->E)"
 is.inus(y)
 x <- "H*T+A*I<->E"
 x <- "C*H+I*T<->E"
-
+check_ccomp(x,y)
 y <- "(C*A+B<->R)*(R+B+X<->Y)*(Y*N+B*n<->L)*(L*a*b+U<->E)"
 x <- "A+B<->E"
-check_comp_asf(x,y)
+check_ccomp(x,y)
 y <- "(A+B<->C)*(C+X<->E)*(E*R*T*x+X*a*b+Y*a*b<->Z)"
 x <- "A+X<->Z"
-
+check_ccomp(x,y)
 
 y <- "(T+R<->Y)*(A+B<->C)*(t*C+a*Y<->E)"
 x <- "A+T<->E"
-check_comp_asf(x,y)
+check_ccomp(x,y)
 y <- "(L+B<->A)*(T+A+R<->Y)*(X+Y<->C)*(T*C+A<->E)"
 x <- "L+T<->E"
-check_comp_asf(x,y)
+check_ccomp(x,y)
 y <- "(A+B<->C)*(T+R<->Y)*(C*Y+c*y<->E)"
 x <-  "t*r+C<->E"
-check_comp_asf(x,y)
+check_ccomp(x,y)
 y <- "(A*X+B<->C)*(C+A*R<->Y)*(C*a*U+Z*y<->E)"
 is.inus(y)
 x <- "R<->E"
-check_comp_asf(x,y)
+check_ccomp(x,y)
 y <- "(A*b+B*a+A*C<->D)*(D+E<->F)"
 x <- "A*b+B*a+B*C<->F"
-check_comp_asf(x,y)
+check_ccomp(x,y)
 y <- "(A+B*F<->C)*(D+B*f<->E)*(C+E<->G)"
 x <- "A+B*F<->G"
 
-check_comp_asf(x,y)
+check_ccomp(x,y)
 
 is.inus(x, selectCases(y))
 y <- "(D*F+a*b<->C)*(F*c<->G)*(B*f+D*c+a*c<->E)"
 x <-"D*f+d*B<->E"
-check_comp_asf(x,y)
+check_ccomp(x,y)
 
 y <- "(C*e*f+E*F*c+F*c*g<->B)*(B*e+E*G*c<->D)*(F*G+d*e<->A)"
 
@@ -48,9 +48,25 @@ check_comp_asf(x,y)
 
 
 ### check_ccomp()
-y <- "(L+B<->A)*(T+A+R<->Y)*(X+Y<->C)*(T*C+A<->E)"
-x <- "(L+B<->Y)*(Y+X<->E)"
+y <- "(L+B<->A)*(T+A+R<->Y)*(X+Y<->C)*(C+U<->E)"
+x <- "(L+B<->Y)*(X+Y<->E)"
 check_ccomp(x,y)
 
+y <- "(L+B<->A)*(T+A+R<->Y)*(X+Y<->C)*(T*C+A<->E)"
+x <- "(L+T<->A)*(X+Y<->E)"
+check_ccomp(x,y)
+
+x <- "L+T<->Y"
+x <- "R+C<->E"
+check_comp_asf(x, y)
+
+y <- "(A+B<->C)*(C+D<->E)*(E+F<->G)*(G+H<->I)"
+x <- "(A+D<->E)*(C+F<->I)"
+check_ccomp(x,y)
+is.inus(x, selectCases(y))
 
 
+target <- "(A + B*D <->C)*(C+D <->G)"
+candidate <- "(A + B*D <-> C)*(C <->G)"
+
+td <- selectCases(target)
